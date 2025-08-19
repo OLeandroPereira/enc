0cee45f21aaf013b6b3b1adb25d325b84f9a78ec
Leandro <leandroppmarques@hotmail.com>
2025-08-19 11:09:58 -0300

    feat: implementação vue router e HomeView para melhoria e organização dos components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div id="app">
-    <Header />
-    <HeroSection />
-    <ParticipateChallengeSection />
-    <RankingSection />
-    <PlayGuideSection />
-    <Footer />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import HeroSection from './components/HeroSection.vue'
-import ParticipateChallengeSection from './components/ParticipateChallengeSection.vue'
-import RankingSection from './components/RankingSection.vue'
-import PlayGuideSection from './components/PlayGuideSection.vue'
-import Footer from './components/Footer.vue'
-
 export default {
-  name: 'App',
-  components: {
-    Header,
-    HeroSection,
-    ParticipateChallengeSection,
-    RankingSection,
-    PlayGuideSection,
-    Footer
-  }
+  name: 'App'
 }
 </script>
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hellmanns-app">
+    <Header />
+    <HeroSection />
+    <ParticipateChallengeSection />
+    <RankingSection />
+    <PlayGuideSection />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import HeroSection from '../components/HeroSection.vue'
+import ParticipateChallengeSection from '../components/ParticipateChallengeSection.vue'
+import RankingSection from '../components/RankingSection.vue'
+import PlayGuideSection from '../components/PlayGuideSection.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Header,
+    HeroSection,
+    ParticipateChallengeSection,
+    RankingSection,
+    PlayGuideSection,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+.hellmanns-app {
+  min-height: 100vh;
+  width: 100%;
+  background-color: #fff;
+  margin: 0;
+  padding: 0;
+}
+</style>
